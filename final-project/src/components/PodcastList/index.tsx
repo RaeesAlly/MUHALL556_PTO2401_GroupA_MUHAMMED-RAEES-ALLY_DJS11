5d7061ed188eae2db8bfe691bc133c2f7f1f192e
RaeesAlly <raees_ally@outlook.com>
2024-11-02 20:40:00 +0200

    add filtering

--- final-project/src/components/PodcastList/index.tsx
+++ final-project/src/components/PodcastList/index.tsx
@@ -3,30 +3,67 @@
 import { PodcastType } from "@/types/podcast";
 import { PodcastCard } from "../Podcast";
 import { useState } from "react";
-import { FaSort, FaSortAlphaDown, FaSortAlphaUp } from "react-icons/fa";
+import { FaSortAlphaDown, FaSortAlphaUp } from "react-icons/fa";
 
 type PodcastListType = {
   podcastList: PodcastType[];
 };
+const categories = [
+  { id: 1, name: "Personal Growth" },
+  { id: 2, name: "Investigative Journalism" },
+  { id: 3, name: "History" },
+  { id: 4, name: "Comedy" },
+  { id: 5, name: "Entertainment" },
+  { id: 6, name: "Business" },
+  { id: 7, name: "Fiction" },
+  { id: 8, name: "News" },
+  { id: 9, name: "Kids and Family" },
+];
 
 export const PodcastList: React.FC<PodcastListType> = ({ podcastList }) => {
   const [isAscending, setIsAscending] = useState(true);
-  const getSortedPodcastList = () => {
-    return podcastList.sort((podcastA, podcastB) => {
-     if (isAscending){
-        return podcastA.title < podcastB.title ? -1 : 1;
-     }
+  const [genre, setGenre] = useState("");
+  const getSortedAndFilteredPodcastList = () => {
+    return podcastList
+      .sort((podcastA, podcastB) => {
+        if (isAscending) {
+          return podcastA.title < podcastB.title ? -1 : 1;
+        }
         return podcastA.title < podcastB.title ? 1 : -1;
-    });
+      })
+      .filter((podcast) => {
+        if (genre === "") {
+          return true;
+        }
+        return podcast.genres.includes(Number(genre));
+      });
   };
   return (
     <section>
-        <section>
-           {isAscending &&<FaSortAlphaDown size={30} className="cursor-pointer" onClick={()=>setIsAscending(!isAscending)}></FaSortAlphaDown>}
-           {!isAscending &&<FaSortAlphaUp size={30} className="cursor-pointer" onClick={()=>setIsAscending(!isAscending)}></FaSortAlphaUp>}
-        </section>
+      <section className="flex gap-10">
+        {isAscending && (
+          <FaSortAlphaDown
+            size={30}
+            className="cursor-pointer"
+            onClick={() => setIsAscending(!isAscending)}
+          ></FaSortAlphaDown>
+        )}
+        {!isAscending && (
+          <FaSortAlphaUp
+            size={30}
+            className="cursor-pointer"
+            onClick={() => setIsAscending(!isAscending)}
+          ></FaSortAlphaUp>
+        )}
+        <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+          <option value={""}>All</option>
+          {categories.map((genre, index: number) => (
+            <option value={genre.id}>{genre.name}</option>
+          ))}
+        </select>
+      </section>
       <section className="mt-10 grid grid-cols-5 gap-20">
-        {getSortedPodcastList().map((podcast, index: number) => (
+        {getSortedAndFilteredPodcastList().map((podcast, index: number) => (
           <PodcastCard key={index} podcast={podcast}></PodcastCard>
         ))}
       </section>
